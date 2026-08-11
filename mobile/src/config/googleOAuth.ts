8663dc2148e05ac66fe0641c8dd4b229efdf9996
Ido Yahav <idoyahav15@gmail.com>
2026-08-12 00:53:55 +0300

    alota

--- mobile/src/config/googleOAuth.ts
+++ mobile/src/config/googleOAuth.ts
@@ -4,9 +4,9 @@ import { makeRedirectUri } from "expo-auth-session";
 
 /** Google OAuth client IDs (public). Backend verifies id_token audience against web + iOS. */
 export const GOOGLE_WEB_CLIENT_ID =
-  "30613281349-rfi4m1fv4rujoc3d5f3vqispl4il8u2b.apps.googleusercontent.com";
+  "858033290834-7vrs7u956crqi2ci1kfgtqjvikd5o66a.apps.googleusercontent.com";
 export const GOOGLE_IOS_CLIENT_ID =
-  "30613281349-i3v2ndea1cg1q1tmqjbsju993rerme7n.apps.googleusercontent.com";
+  "858033290834-bhhndi5aqeqtj9r4t0fd3u5vs4aq9snd.apps.googleusercontent.com";
 /** Console > Credentials > "Android" client (android.package + signing SHA-1). Empty until created. */
 export const GOOGLE_ANDROID_CLIENT_ID = "";
 
